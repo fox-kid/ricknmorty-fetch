57661f0d7d172ac5b1cc25cb1b23df0c62661be5
fox-kid <annalezhava39@gmail.com>
2022-10-06 19:01:41 +0400

    Add html page and functions on button clicks

---
+++ README.md
@@ -0,0 +1,2 @@
+# To make the project work, move to project directory and run
+npm install
--- index.js
+++ index.js
@@ -1,31 +1,38 @@
-import fetch from "node-fetch";
+'use-strict';
+// import fetch from "node-fetch";
 
-const URL = "https://rickandmortyapi.com/api"
-
-
-
+const URL = "https://rickandmortyapi.com/api";
 
 
 async function getData() {
-    let allData = await fetch(URL).then((response) => response.json()).then((data) => console.log(data)).catch((err) => console.log(err));
+    let allData = await fetch(URL).then((response) => response.json()).then((data) => data).catch((err) => console.log(err));
+
+    console.log('The list of API URLs here')
+    console.log(allData);
 
     return allData;
 } 
 
-getData(); // Will give us general data about Rick and Morty
+// getData(); // Will give us general data about Rick and Morty
 
 
 
 
+async function getEpisodeByPage(page = 1) {
+    let episode = await fetch(`${URL}/episode/?page=${page}`).then((response) => response.json()).then((data) => data).catch((err) => console.log(err));
 
-async function getEpisode(page = 1) {
-    let episode = await fetch(`${URL}/episode/?page=${page}`).then((response) => response.json()).then((data) => console.log(data)).catch((err) => console.log(err));
+    if(page == 1) {
+        console.log('The list of episodes by default value of a page');
+    } else {
+        console.log('The list of episodes on a 3rd page')
+    }
+    console.log(episode.results);
 
-    return episode;
+    return episode.results;
 }
 
-getEpisode(); // Will give us the first (default) page of episodes list
-getEpisode(4); // Will give us the fourth page of episodes list
+// getEpisode(); // Will give us the first (default) page of episodes list
+// getEpisode(3); // Will give us the fourth page of episodes list
 
 
 
@@ -35,13 +42,16 @@ getEpisode(4); // Will give us the fourth page of episodes list
 // the default first page or an error message saying you should provide an ID.
 // You should either add "${id}" as in template literal or "${id ? id : ""}"
 
-async function getEpisode(id) {
-    let episode = await fetch(`${URL}/episode/${id ? id : ""}`).then((response) => response.json()).then((data) => console.log(data)).catch((err) => console.log(err));
+async function getEpisodeById(id) {
+    let episode = await fetch(`${URL}/episode/${id ? id : ""}`).then((response) => response.json()).then((data) => data).catch((err) => console.log(err));
+
+    console.log(`This is the episode by ID ${id}`)
+    console.log(episode);
 
     return episode;
 }
 
-getEpisode(5)
+// getEpisode(5);
 
 
 
@@ -57,17 +67,19 @@ async function getCharacter(params, ids) {
     let genderQuery = params && params.gender ? `gender=${params.gender}&` : "";
 
 
-    let character = await fetch(`${URL}/character/${ids ? ids : ""}?${nameQuery}${statusQuery}${genderQuery}`).then((response) => response.json()).then((data) => console.log(data)).catch((err) => console.log(err));
+    let character = await fetch(`${URL}/character/${ids ? ids : ""}?${nameQuery}${statusQuery}${genderQuery}`).then((response) => response.json()).then((data) => data).catch((err) => console.log(err));
+    
+    console.log('The character(s) with specified parameters or ID:');
+    console.log(ids == undefined ? character.results : character);
     
     return character;
 }
 
 
-getCharacter(undefined, 9); // Gives an output of character with ID 9
-getCharacter(undefined, [3, 17, 193]); // Gives an output of an array of characters with IDs 3, 17, 193
-getCharacter({name: "rick", status: "alive", gender: "female"}); // Gives an output of the first of page of characters list
-getCharacter({gender: "female"}); // Gives an output of characters with gender key "female" value
-
+// getCharacter(undefined, 9); // Gives an output of character with ID 9
+// getCharacter(undefined, [3, 17, 193]); // Gives an output of an array of characters with IDs 3, 17, 193
+// getCharacter({name: "rick", status: "alive", gender: "female"}); // Gives an output of a character(s) with these parameters
+// getCharacter({gender: "female"}); // Gives an output of characters with gender key "female" value
 
 
 
@@ -81,12 +93,15 @@ async function getLocation(params, ids) {
     let dimensionQuery = params && params.dimension ? `dimension=${params.dimension}&` : "";
 
 
-    let location = await fetch(`${URL}/location/${ids ? ids : ""}?${nameQuery}${typeQuery}${dimensionQuery}`).then((response) => response.json()).then((data) => console.log(data)).catch((err) => console.log(err));
+    let location = await fetch(`${URL}/location/${ids ? ids : ""}?${nameQuery}${typeQuery}${dimensionQuery}`).then((response) => response.json()).then((data) => data).catch((err) => console.log(err));
+
+    console.log('The list of locations:');
+    console.log(ids == undefined ? location.results : location);
 
     return location;
 }
 
-getLocation(); // First page of locations list, 20 items
-getLocation({ name: "Citadel" }); // Location with the name "Citadel" in it
-getLocation({ type: "planet", dimension: "unknown" }); // Locations that have type "planet" and dimension "unknown"
-getLocation(undefined, [5, 19, 28]); // Array of locations by id 5, 19, 28
+// getLocation(); // First page of locations list, 20 items
+// getLocation({ name: "Citadel" }); // Location with the name "Citadel" in it
+// getLocation({ type: "planet", dimension: "unknown" }); // Locations that have type "planet" and dimension "unknown"
+// getLocation(undefined, [5, 19, 28]); // Array of locations by id 5, 19, 28
